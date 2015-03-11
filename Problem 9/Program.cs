693c0ce0f8a9baac91a60e0dd6a4500d9d63a423
Ilgaz Altınören <ilgaz@altinoren.com>
2015-03-11 18:47:49 +0200

    :boom: I have completed Problem 9

---
+++ Problem 9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 333; i++)
+            {
+                for (int j = i+1; j < 500; j++)
+                {
+                    var k = 1000 - (i + j);
+                    if(i*i+j*j == k*k)
+                    {
+                        Console.WriteLine("i:{0}, j:{1}, k:{2}, product:{3}", i, j, k, i*j*k);
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
